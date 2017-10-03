39c59799f800722a97d7a4977a6509cf812f501e
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-03 01:01:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var name = "Joe"
-var height = "74"
+var name = "Joe";
+var height = "74";
 
 
 if(name == Joe){
-  console.log("The name is correct")
+  console.log("The name is correct");
   console.log("Joe");
 } else {
-  console.log("Expected the name to be Susan")
+  console.log("Expected the name to be Susan");
 }
 
 
