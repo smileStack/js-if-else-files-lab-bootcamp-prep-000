fa927ad54caf867239d1d3e8c9d8922ca9c05102
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-03 00:59:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var name = "Joe"
 var height = "74"
 
 
+if(name == Joe){
+  console.log("The name is correct")
+  console.log("Joe");
+} else {
+  console.log("Expected the name to be Susan")
+}
+
+  
+
 // Don't worry about this
 module.exports = { name, height
 }
